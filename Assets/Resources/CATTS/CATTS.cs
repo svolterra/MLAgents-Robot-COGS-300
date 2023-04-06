f3a08446d34611dc48577d93f269930347c2d69e
svolterra <sarahkalmm1902@gmail.com>
2023-04-06 11:53:26 -0700

    first commit

---
+++ Assets/Resources/CATTS/CATTS.cs
@@ -0,0 +1,313 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class CATTS : CogsAgent
+{
+    // ------------------BASIC MONOBEHAVIOR FUNCTIONS-------------------
+    
+    // Initialize values
+    protected override void Start()
+    {
+        base.Start();
+        AssignBasicRewards();
+    }
+
+    // For actual actions in the environment (e.g. movement, shoot laser)
+    // that is done continuously
+    protected override void FixedUpdate() {
+        base.FixedUpdate();
+        
+        LaserControl();
+        // Movement based on DirToGo and RotateDir
+        moveAgent(dirToGo, rotateDir);
+        Debug.Log(GetCumulativeReward());
+    }
+
+
+    
+    // --------------------AGENT FUNCTIONS-------------------------
+
+    // Get relevant information from the environment to effectively learn behavior
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Agent velocity in x and z axis 
+        var localVelocity = transform.InverseTransformDirection(rBody.velocity);
+        sensor.AddObservation(localVelocity.x);
+        sensor.AddObservation(localVelocity.z);
+
+        // Time remaning
+        sensor.AddObservation(timer.GetComponent<Timer>().GetTimeRemaning());  
+
+        // Agent's current rotation
+        var localRotation = transform.rotation;
+        sensor.AddObservation(transform.rotation.y);
+
+        // Agent and home base's position
+        sensor.AddObservation(this.transform.localPosition);
+        sensor.AddObservation(baseLocation.localPosition);
+
+        // for each target in the environment, add: its position, whether it is being carried,
+        // and whether it is in a base
+        foreach (GameObject target in targets){
+            sensor.AddObservation(target.transform.localPosition);
+            sensor.AddObservation(target.GetComponent<Target>().GetCarried());
+            sensor.AddObservation(target.GetComponent<Target>().GetInBase());
+        }
+        
+        // Whether the agent is frozen
+        sensor.AddObservation(IsFrozen());
+    }
+
+    // For manual override of controls. This function will use keyboard presses to simulate output from your NN 
+    public override void Heuristic(in ActionBuffers actionsOut)
+{
+        var discreteActionsOut = actionsOut.DiscreteActions;
+        discreteActionsOut[0] = 0; //Simulated NN output 0
+        discreteActionsOut[1] = 0; //....................1
+        discreteActionsOut[2] = 0; //....................2
+        discreteActionsOut[3] = 0; //....................3
+        discreteActionsOut[4] = 0;
+
+       
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            discreteActionsOut[0] = 1;
+        }       
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            discreteActionsOut[0] = 2;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            discreteActionsOut[1] = 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            discreteActionsOut[1] = 2;
+        }
+        
+        //Shoot
+        if (Input.GetKey(KeyCode.Space)){
+            discreteActionsOut[2] = 1;
+        }
+
+        //GoToNearestTarget
+        if (Input.GetKey(KeyCode.A)){
+            discreteActionsOut[3] = 1;
+        }
+
+        //GoBackToBase
+        if (Input.GetKey(KeyCode.Z)) {
+            discreteActionsOut[4] = 1;
+        }
+    }
+
+        // What to do when an action is received (i.e. when the Brain gives the agent information about possible actions)
+        public override void OnActionReceived(ActionBuffers actions){
+
+        int forwardAxis = (int)actions.DiscreteActions[0]; //NN output 0
+        int rotateAxis = (int)actions.DiscreteActions[1]; 
+        int shootAxis = (int)actions.DiscreteActions[2]; 
+        int goToTargetAxis = (int)actions.DiscreteActions[3];
+        int goToBaseAxis = (int)actions.DiscreteActions[4];
+
+        MovePlayer(forwardAxis, rotateAxis, shootAxis, goToTargetAxis, goToBaseAxis);
+    }
+
+
+// ----------------------ONTRIGGER AND ONCOLLISION FUNCTIONS------------------------
+    // Called when object collides with or trigger (similar to collide but without physics) other objects
+    protected override void OnTriggerEnter(Collider collision)
+    {
+        base.OnTriggerEnter(collision);
+        
+        if (collision.gameObject.CompareTag("HomeBase") && collision.gameObject.GetComponent<HomeBase>().team == GetTeam())
+        {
+            //Add rewards here
+            AddReward(0.005f);
+        }
+    }
+
+    protected override void OnCollisionEnter(Collision collision) 
+    {
+        //target is not in my base and is not being carried and I am not frozen
+        if (collision.gameObject.CompareTag("Target") && collision.gameObject.GetComponent<Target>().GetInBase() != GetTeam() && collision.gameObject.GetComponent<Target>().GetCarried() == 0 && !IsFrozen())
+        {
+            //Add rewards here
+            AddReward(0.001f);
+        }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //Add rewards here
+            AddReward(-0.5f);
+        }
+        
+        base.OnCollisionEnter(collision);
+    }
+
+
+
+    //  --------------------------HELPERS---------------------------- 
+     private void AssignBasicRewards() {
+        rewardDict = new Dictionary<string, float>();
+
+        rewardDict.Add("frozen", -5f);
+        rewardDict.Add("shooting-laser", 0.01f);
+        rewardDict.Add("hit-enemy", 7f);
+        rewardDict.Add("dropped-one-target", -0.5f);
+        rewardDict.Add("dropped-targets", -5f);
+    }
+    
+    private void MovePlayer(int forwardAxis, int rotateAxis, int shootAxis, int goToTargetAxis, int goToBaseAxis)
+    {
+        dirToGo = Vector3.zero;
+        rotateDir = Vector3.zero;
+
+        Vector3 forward = transform.forward;
+        Vector3 backward = -transform.forward;
+        Vector3 right = transform.up;
+        Vector3 left = -transform.up;
+
+        int numBallsInBase = myBase.GetComponent<HomeBase>().GetCaptured();
+
+        //fowardAxis: 
+            // 0 -> do nothing
+            // 1 -> go forward
+            // 2 -> go backward
+        if (forwardAxis == 0){
+            //do nothing. 
+        }
+        else if (forwardAxis == 1){
+            dirToGo = forward;
+        }
+        else if (forwardAxis == 2){
+            dirToGo = backward;
+        }
+
+        //rotateAxis: 
+            // 0 -> do nothing
+            // 1 -> go right
+            // 2 -> go left
+        if (rotateAxis == 0){
+            //do nothing
+        } else if (rotateAxis == 1) {
+            rotateDir = right;
+        } else if (rotateAxis == 2) {
+            rotateDir = left;
+        }
+        
+        //shoot
+        if (shootAxis == 1 || DetectNearbyEnemy()) {
+            SetLaser(true);
+        }
+        else {
+            SetLaser(false);
+        }
+
+        //go to the nearest target
+        if (goToTargetAxis == 1){
+            GoToNearestTarget();
+        }
+
+        /* automatically go back to base location under any of the following conditions:
+            - Z key is pressed
+            - 10 seconds remain on the timer and agent has enough balls to win the match
+            - The agent is carrying exactly three balls
+            - The agent has successfully captured 6 balls --> begin defense 
+        */
+        if (goToBaseAxis == 1 
+            || (timer.GetComponent<Timer>().GetTimeRemaning() < 10 &&  numBallsInBase >= 5)
+            || GetCarrying() == 3  
+            || numBallsInBase == 6
+            ) 
+        {
+            GoToBase();
+        }
+
+        //add big reward when agent returns to base carrying more than one ball
+        if (DistanceToBase() == 0 && GetCarrying() > 1) {
+            AddReward(5f);
+        }
+
+        //add big reward if agent has captured 5 or more target balls
+        if (numBallsInBase >= 5) {
+            AddReward(30f);
+        }
+
+        //end the episode once the time runs out
+        if (timer.GetComponent<Timer>().GetTimeRemaning() == 0) {
+            EndEpisode();
+        }
+    }
+
+    // Go to home base
+    private void GoToBase(){
+        TurnAndGo(GetYAngle(myBase));
+    }
+
+    // Go to the nearest target
+    private void GoToNearestTarget(){
+        GameObject target = GetNearestTarget();
+        if (target != null){
+            float rotation = GetYAngle(target);
+            TurnAndGo(rotation);
+            AddReward(3f);
+        }        
+    }
+
+    // Rotate and go in specified direction
+    private void TurnAndGo(float rotation){
+
+        if(rotation < -5f){
+            rotateDir = transform.up;
+        }
+        else if (rotation > 5f){
+            rotateDir = -transform.up;
+        }
+        else {
+            dirToGo = transform.forward;
+        }
+    }
+
+    // return reference to nearest target
+    protected GameObject GetNearestTarget(){
+        float distance = 200;
+        GameObject nearestTarget = null;
+        foreach (var target in targets)
+        {
+            float currentDistance = Vector3.Distance(target.transform.localPosition, transform.localPosition);
+            if (currentDistance < distance && target.GetComponent<Target>().GetCarried() == 0 && target.GetComponent<Target>().GetInBase() != team){
+                distance = currentDistance;
+                nearestTarget = target;
+            }
+        }
+        return nearestTarget;
+    }
+
+    private float GetYAngle(GameObject target) {
+        
+       Vector3 targetDir = target.transform.position - transform.position;
+       Vector3 forward = transform.forward;
+
+      float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+      return angle; 
+        
+    }
+
+    private bool DetectNearbyEnemy() {
+    float laserLength = 20;
+    var rayDir = laserLength * transform.forward;
+
+    RaycastHit hit;
+    if (Physics.SphereCast(transform.position, 0.25f, rayDir, out hit, laserLength,1, QueryTriggerInteraction.Ignore)) {
+            return hit.collider.gameObject.CompareTag("Player");
+        } else {
+            return false;
+        }
+    }
+}
